a8771504bcc14791de111225c5a793137fb440f2
jizzoe <jizzoerice@gmail.com>
2015-01-29 16:00:36 -0500

    changed organization to chef-tutorial

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,8 +5,8 @@ log_level                :info
 log_location             STDOUT
 node_name                "jizzoe"
 client_key               "#{current_dir}/jizzoe.pem"
-validation_client_name   "swing-tech-validator"
-validation_key           "#{current_dir}/swing-tech-validator.pem"
+validation_client_name   "chef-tutorial-validator"
+validation_key           "#{current_dir}/chef-tutorial-validator.pem"
 chef_server_url          "https://ec2-54-173-160-1.compute-1.amazonaws.com/organizations/swing-tech"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
